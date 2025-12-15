19b86ed7a61832c4b86b32e3392eafa3b852bbd4
jalenchen0 <jalenchen2008@gmail.com>
2025-12-15 11:02:13 -0500

    updated recipes.py with new api calls

--- app/recipes.py
+++ app/recipes.py
@@ -17,3 +17,41 @@ def request_drink(id):
     except requests.exceptions.RequestException as e:
         print(e)
 
+def request_position(timestamp):
+    url = f"https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps={timestamp}"
+    # timestamp is in unix/epoch time
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        result = response.json()[0]
+
+        coordinates = [result["latitude"], result["longitude"]]
+
+        return coordinates
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
+def request_value(timestamp):
+    url = f"https://www.goldapi.io/api/XAU/USD/{timestamp}"
+    # timestamp is in YYYYMMDD
+
+    api_key = "goldapi-citmsmixc4q0y-io" #read key_GoldAPI.txt later
+
+    headers = {
+        "x-access-token": api_key,
+        "Content-Type": "application/json"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        result = response.text
+        print(result)
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+request_value(20251214)
