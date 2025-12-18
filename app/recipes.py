25ec55634ec0881a2d4aabe6086c74b499596635
jalenchen0 <jalenchen2008@gmail.com>
2025-12-18 11:00:10 -0500

    changed request_position() to request_coordinates() and created request_country()

--- app/recipes.py
+++ app/recipes.py
@@ -4,90 +4,90 @@
 
 import requests
 
-ingredient_data = {"Añejo Rum" : {"flavor": "alcohol"}, 
-                   "Tia Maria" : {"flavor": "alcohol"}, 
-                   "Vodka" : {"flavor": "alcohol"}, 
-                   'Orange Juice' : {"flavor": "fruity"}, 
-                   'Lemon Juice' : {"flavor": "sour"}, 
-                   'Light Rum' : {"flavor": "alcohol"}, 
-                   'Lime' : {"flavor": "sour"}, 
-                   'Sugar' : {"flavor": "sweet"}, 
-                   'Mint' : {"flavor": "other"}, 
-                   'Jack Daniels' : {"flavor": "alcohol"}, 
-                   'Midori Melon Liqueur' : {"flavor": "fruit"}, 
-                   'Sour Mix' : {"flavor": "sour"}, 
-                   'Dark Rum' : {"flavor": "alcohol"}, 
-                   'Pineapple Juice' : {"flavor": "fruity"}, 
-                   'Grenadine' : {"flavor": "sweet"}, 
-                   'Sugar Syrup' : {"flavor": "sweet"}, 
-                   'Angostura Bitters' : {"flavor": "other"}, 
-                   'Rum' : {"flavor": "alcohol"}, 
-                   'Grapefruit Juice' : {"flavor": "fruity"}, 
-                   'Maraschino Liqueur' : {"flavor": "sweet"}, 
-                   'Lime Juice' : {"flavor": "sour"}, 
-                   'Brandy' : {"flavor": "alcohol"}, 
-                   'lemon' : {"flavor": "sour"}, 
-                   'Powdered Sugar' : {"flavor": "sweet"}, 
-                   'Cherry' : {"flavor": "fruity"}, 
-                   'Sweet Vermouth' : {"flavor": "alcohol"}, 
-                   'Bourbon' : {"flavor": "alcohol"}, 
-                   'Ice' : {"flavor": "other"}, 
-                   'Maraschino Cherry' : {"flavor": "sweet"}, 
-                   'Orange Peel' : {"flavor": "fruity"}, 
-                   'Coconut Milk' : {"flavor": "milky"}, 
-                   'Pineapple' : {"flavor": "fruity"}, 
-                   'Tequila' : {"flavor": "alcohol"}, 
-                   'Gin' : {"flavor": "alcohol"}, 
-                   'Coca-Cola' : {"flavor": "sweet"}, 
-                   'Lemon Peel' : {"flavor": "sour"}, 
-                   'Sherry' : {"flavor": "alcohol"}, 
-                   'Orange Bitters': {"flavor": "other"}, 
-                   'Lemonade' : {"flavor": "fruity"}, 
-                   'Water' : {"flavor": "other"}, 
-                   'Ginger' : {"flavor": "other"}, 
-                   'Guava juice' : {"flavor": "fruity"}, 
-                   'Peach Nectar' : {"flavor": "sweet"}, 
-                   'Brown Sugar' : {"flavor": "sweet"}, 
-                   'Cinnamon' : {"flavor": "other"}, 
-                   'Cloves' : {"flavor": "other"}, 
-                   'Passion fruit juice' : {"flavor": "fruity"}, 
-                   'Ginger Ale' : {"flavor": "sweet"}, 
-                   'Soda Water' : {"flavor": "other"}, 
-                   'Mango' : {"flavor": "fruity"}, 
-                   'Orange' : {"flavor": "fruity"}, 
-                   'Apple Cider' : {"flavor": "fruity"}, 
-                   'Allspice' : {"flavor": "other"}, 
-                   'Nutmeg' : {"flavor": "other"}, 
-                   'Light Cream' : {"flavor": "milky"}, 
-                   'Egg White' : {"flavor": "milky"}, 
-                   'Coffee Liqueur' : {"flavor": "milky"}, 
-                   'Heavy cream' : {"flavor": "milky"}, 
-                   'Chocolate Liqueur' : {"flavor": "milky"}, 
-                   'Amaretto' : {"flavor": "alcohol"}, 
-                   'Chocolate Sauce' : {"flavor": "milky"}, 
-                   'Salted Chocolate' : {"flavor": "sweet"}, 
-                   'Scotch' : {"flavor": "alcohol"}, 
-                   'Curacao' : {"flavor": "alcohol"}, 
-                   'Half-and-half' : {"flavor": "milky"}, 
-                   'Condensed Milk' : {"flavor": "sweet"}, 
-                   'Coconut Syrup' : {"flavor": "sweet"}, 
-                   'Chocolate Syrup' : {"flavor": "milky"}, 
-                   'Kahlua' : {"flavor": "alcohol"}, 
-                   'Baileys Irish Cream' : {"flavor": "alcohol"}, 
-                   'Vanilla Ice-Cream' : {"flavor": "milky"}, 
-                   'Oreo cookie' : {"flavor": "sweet"}, 
-                   'Dark Creme De Cacao' : {"flavor": "milky"}, 
-                   'Coffee' : {"flavor": "milky"}, 
-                   'Banana Liqueur' : {"flavor": "sweet"}, 
-                   'Creme De Cacao' : {"flavor": "milky"}, 
-                   'Chocolate Ice-cream' : {"flavor": "milky"}, 
-                   'Chocolate Milk' : {"flavor": "milky"}, 
-                   'Whipped Cream' : {"flavor": "milky"}, 
-                   'Banana' : {"flavor": "fruity"}, 
-                   'Milk' : {"flavor": "milky"}, 
-                   'Vanilla' : {"flavor": "sweet"}, 
-                   'Chocolate' : {"flavor": "sweet"}, 
-                   'Espresso' : {"flavor": "milky"}, 
+ingredient_data = {"Añejo Rum" : {"flavor": "alcohol"},
+                   "Tia Maria" : {"flavor": "alcohol"},
+                   "Vodka" : {"flavor": "alcohol"},
+                   'Orange Juice' : {"flavor": "fruity"},
+                   'Lemon Juice' : {"flavor": "sour"},
+                   'Light Rum' : {"flavor": "alcohol"},
+                   'Lime' : {"flavor": "sour"},
+                   'Sugar' : {"flavor": "sweet"},
+                   'Mint' : {"flavor": "other"},
+                   'Jack Daniels' : {"flavor": "alcohol"},
+                   'Midori Melon Liqueur' : {"flavor": "fruit"},
+                   'Sour Mix' : {"flavor": "sour"},
+                   'Dark Rum' : {"flavor": "alcohol"},
+                   'Pineapple Juice' : {"flavor": "fruity"},
+                   'Grenadine' : {"flavor": "sweet"},
+                   'Sugar Syrup' : {"flavor": "sweet"},
+                   'Angostura Bitters' : {"flavor": "other"},
+                   'Rum' : {"flavor": "alcohol"},
+                   'Grapefruit Juice' : {"flavor": "fruity"},
+                   'Maraschino Liqueur' : {"flavor": "sweet"},
+                   'Lime Juice' : {"flavor": "sour"},
+                   'Brandy' : {"flavor": "alcohol"},
+                   'lemon' : {"flavor": "sour"},
+                   'Powdered Sugar' : {"flavor": "sweet"},
+                   'Cherry' : {"flavor": "fruity"},
+                   'Sweet Vermouth' : {"flavor": "alcohol"},
+                   'Bourbon' : {"flavor": "alcohol"},
+                   'Ice' : {"flavor": "other"},
+                   'Maraschino Cherry' : {"flavor": "sweet"},
+                   'Orange Peel' : {"flavor": "fruity"},
+                   'Coconut Milk' : {"flavor": "milky"},
+                   'Pineapple' : {"flavor": "fruity"},
+                   'Tequila' : {"flavor": "alcohol"},
+                   'Gin' : {"flavor": "alcohol"},
+                   'Coca-Cola' : {"flavor": "sweet"},
+                   'Lemon Peel' : {"flavor": "sour"},
+                   'Sherry' : {"flavor": "alcohol"},
+                   'Orange Bitters': {"flavor": "other"},
+                   'Lemonade' : {"flavor": "fruity"},
+                   'Water' : {"flavor": "other"},
+                   'Ginger' : {"flavor": "other"},
+                   'Guava juice' : {"flavor": "fruity"},
+                   'Peach Nectar' : {"flavor": "sweet"},
+                   'Brown Sugar' : {"flavor": "sweet"},
+                   'Cinnamon' : {"flavor": "other"},
+                   'Cloves' : {"flavor": "other"},
+                   'Passion fruit juice' : {"flavor": "fruity"},
+                   'Ginger Ale' : {"flavor": "sweet"},
+                   'Soda Water' : {"flavor": "other"},
+                   'Mango' : {"flavor": "fruity"},
+                   'Orange' : {"flavor": "fruity"},
+                   'Apple Cider' : {"flavor": "fruity"},
+                   'Allspice' : {"flavor": "other"},
+                   'Nutmeg' : {"flavor": "other"},
+                   'Light Cream' : {"flavor": "milky"},
+                   'Egg White' : {"flavor": "milky"},
+                   'Coffee Liqueur' : {"flavor": "milky"},
+                   'Heavy cream' : {"flavor": "milky"},
+                   'Chocolate Liqueur' : {"flavor": "milky"},
+                   'Amaretto' : {"flavor": "alcohol"},
+                   'Chocolate Sauce' : {"flavor": "milky"},
+                   'Salted Chocolate' : {"flavor": "sweet"},
+                   'Scotch' : {"flavor": "alcohol"},
+                   'Curacao' : {"flavor": "alcohol"},
+                   'Half-and-half' : {"flavor": "milky"},
+                   'Condensed Milk' : {"flavor": "sweet"},
+                   'Coconut Syrup' : {"flavor": "sweet"},
+                   'Chocolate Syrup' : {"flavor": "milky"},
+                   'Kahlua' : {"flavor": "alcohol"},
+                   'Baileys Irish Cream' : {"flavor": "alcohol"},
+                   'Vanilla Ice-Cream' : {"flavor": "milky"},
+                   'Oreo cookie' : {"flavor": "sweet"},
+                   'Dark Creme De Cacao' : {"flavor": "milky"},
+                   'Coffee' : {"flavor": "milky"},
+                   'Banana Liqueur' : {"flavor": "sweet"},
+                   'Creme De Cacao' : {"flavor": "milky"},
+                   'Chocolate Ice-cream' : {"flavor": "milky"},
+                   'Chocolate Milk' : {"flavor": "milky"},
+                   'Whipped Cream' : {"flavor": "milky"},
+                   'Banana' : {"flavor": "fruity"},
+                   'Milk' : {"flavor": "milky"},
+                   'Vanilla' : {"flavor": "sweet"},
+                   'Chocolate' : {"flavor": "sweet"},
+                   'Espresso' : {"flavor": "milky"},
                    'Egg Yolk' : {"flavor": "milky"}}
 
 
@@ -121,7 +121,7 @@ a2 = [13554, 88538, 99612, 111282, 330969, 334642, 375884, 579225, 674837]
 b2 = [42501, 264361, 434283, 577385, 934558]
 
 
-def request_position(timestamp):
+def request_coordinates(timestamp):
     url = f"https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps={timestamp}"
     # timestamp is in unix/epoch time
 
@@ -137,6 +137,25 @@ def request_position(timestamp):
     except requests.exceptions.RequestException as e:
         print(e)
 
+def request_country(coordinates):
+    url = f"https://api.wheretheiss.at/v1/coordinates/{coordinates[0]},{coordinates[1]}"
+    # use coordinates provided from request_coordinates()
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        result = response.json()
+
+        country = result["country_code"]
+        if country == "??":
+            country = "International Waters"
+        # default if ISS is above an ocean / international waters
+
+        return country
+    except requests.exceptions.RequestException as e:
+        print(e)
+
 
 def request_value(timestamp):
     url = f"https://www.goldapi.io/api/XAU/USD/{timestamp}"
@@ -148,8 +167,9 @@ def request_value(timestamp):
     try:
         with open("keys/key_GoldAPI.txt", "r") as f:
             api_key = f.read().strip()
-    except FileNotFoundError:
-        print("GoldAPI key not found")
+            print(f.read().strip())
+    except FileNotFoundError as e:
+        print(e)
     '''
 
     api_key = "goldapi-citmsmixc4q0y-io" #read key_GoldAPI.txt later
@@ -168,4 +188,4 @@ def request_value(timestamp):
     except requests.exceptions.RequestException as e:
         print(e)
 
-# request_value(20251216)
+request_value(20251216)
